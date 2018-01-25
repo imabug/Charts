5dda3e4323e21bcee8ef2b538d75ef2287fbaf30
Eugene Mah <eugenemah@gmail.com>
2018-01-24 19:37:41 -0500

    Add Google calendar chart

--- resources/views/google/calendar.blade.php
+++ resources/views/google/calendar.blade.php
@@ -1,26 +1,27 @@
 <script type="text/javascript">
-    chart = google.charts.setOnLoadCallback(drawChart)
+    google.charts.load("current", {packages:["calendar"]});
+    chart = google.charts.setOnLoadCallback(drawChart);
     var {{ $model->id }};
     function drawChart() {
-        var dataTable = google.visualization.DataTable();
+        var dataTable = new google.visualization.DataTable();
         dataTable.addColumn({ type: 'date', id: 'Date'});
-        dataTAble.addColumn({ type: 'number', id: '{!! $model->element_label !!}'});
+        dataTable.addColumn({ type: 'number', id: '{!! $model->element_label !!}'});
         dataTable.addRows([
                 @for ($i = 0; $i < count($model->values); $i++)
-                    ["{!! $model->labels[$i] !!}", {{ $model->values[$i] }}],
+                    [new Date("{!! $model->labels[$i] !!}"), {{ $model->values[$i] }}],
                 @endfor
         ]);
 
         var options = {
+            @include('charts::google.titles')
             @include('charts::_partials.dimension.js')
             fontSize: 12,
-            @include('charts::google.titles')
             legend: { position: 'top', alignment: 'end' }
         };
 
-        {{ $model->id }} = new google.visualization.Calendar(document.getElementById("{{ $model->id }}"))
+        {{ $model->id }} = new google.visualization.Calendar(document.getElementById("{{ $model->id }}"));
 
-        {{ $model->id }}.draw(data, options)
+        {{ $model->id }}.draw(dataTable, options);
     }
 </script>
 
